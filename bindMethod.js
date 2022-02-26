b87ed95049f9941b2e9dd7018bae974b32336828
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-26 08:02:15 +0600

    bind method file

---
+++ bindMethod.js
@@ -0,0 +1,30 @@
+const user1 = {
+    id: 180230086,
+    name: "Rafat Hossain",
+    money: 10000,
+    isRich: true,
+    buyClothes: function(getExpenses){
+        this.money = this.money - getExpenses;
+        console.log(this);
+    }
+}
+
+const user2 = {
+    id: 180230106,
+    name: "Jubair",
+    money: 5000,
+    isRich: false
+}
+
+const user3 = {
+    id: 180230085,
+    name: "Sagor",
+    money: 15000,
+    isRich: true
+}
+
+// user1.buyClothes(1000);
+const user2BuyClothes = user1.buyClothes.bind(user2);
+user2BuyClothes(1500);
+const user3BuyClothes = user1.buyClothes.bind(user3);
+user3BuyClothes(10000);
